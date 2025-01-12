5a39b9bc9100c2d3f5877dd091ed2ef7a08932d4
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-11 17:38:45 -0800

    cannot figure out nested includes due to relative path

--- git-submodule.mk
+++
@@ -1,4 +0,0 @@
-.PHONY: git-submodule-update
-
-git-submodule-update:
-	git subodule foreach 'git pull origin main && git submodule update --init'
--- git.mk
+++ git.mk
@@ -1,2 +1,14 @@
-include git-hooks.mk
-include git-submodule.mk
+GIT_HOOKS_DIR = .git-hooks
+
+.PHONY: git-hooks-apply git-submodule-update
+
+git-hooks-apply:
+	@[ -n '$(GIT_HOOKS_DIR)' ] || (echo 'GIT_HOOKS_DIR not specified in makefile'; exit 1)
+	@for hook in $(notdir $(wildcard ./$(GIT_HOOKS_DIR)/*)); do \
+		echo "replacing existing hook: $$hook" && \
+		rm -f ".git/hooks/$$hook" && \
+		ln -s "../../$(GIT_HOOKS_DIR)/$$hook" ".git/hooks/$$hook"; \
+	done
+
+git-submodule-update:
+	git submodule foreach 'git pull origin main && git submodule update --init'
