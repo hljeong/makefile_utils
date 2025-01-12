77673e1751361192ba6be65033a199c92ef612b3
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-12 01:18:54 -0800

    more checks in git-hook-apply

--- git.mk
+++ git.mk
@@ -1,25 +1,38 @@
-GIT_HOOKS_DIR = .git-hooks
+GIT_HOOK_DIR = .git-hooks
 
-.PHONY: git-hooks-apply git-submodule-update git-submodule-reset
+.PHONY: git-hook-apply git-hook-git-hook-dir-defined git-submodule-update git-submodule-reset
 
-# todo: report if GIT_HOOKS_DIR does not exist or there are no hooks to be applied
-git-hooks-apply:
-	@ [ -n '$(GIT_HOOKS_DIR)' ] || (echo 'GIT_HOOKS_DIR not specified in makefile'; exit 1)
+git-hook-git-hook-dir-defined:
+	@ [ -n '$(GIT_HOOK_DIR)' ] || (echo 'GIT_HOOK_DIR not specified in makefile'; exit 1)
+
+git-hook-apply:
+	@ [ -d './$(GIT_HOOK_DIR)' ] || (echo './$(GIT_HOOK_DIR) does not exist, no git hooks will be installed')
 	@ \
-	for hook in $(notdir $(wildcard ./$(GIT_HOOKS_DIR)/*)); do \
+	[ -n "$$(ls -A ./$(GIT_HOOK_DIR))" ] || (echo './$(GIT_HOOK_DIR) is empty, no git hooks will be installed'); \
+	for hook in $(notdir $(wildcard ./$(GIT_HOOK_DIR)/*)); do \
+		if [ -d "./$(GIT_HOOK_DIR)/$$hook" ]; then \
+			echo "cannot apply git hook for $$hook: ./$(GIT_HOOK_DIR)/$$hook is a directory"; \
+			exit 1; \
+		fi; \
+		\
+		if [ ! -x "./$(GIT_HOOK_DIR)/$$hook" ]; then \
+			echo "cannot apply git hook for $$hook: ./$(GIT_HOOK_DIR)/$$hook is not executable"; \
+			exit 1; \
+		fi; \
+		\
 		if [ -d ".git/hooks/$$hook" ]; then \
-			echo "cannot apply hook for $$hook: .git/hooks/$$hook is a directory"; \
+			echo "cannot apply git hook for $$hook: .git/hooks/$$hook is a directory"; \
 			exit 1; \
 		fi; \
 		\
 		if [ -f ".git/hooks/$$hook" ] || [ -L ".git/hooks/$$hook" ]; then \
-			echo "replacing existing hook: $$hook"; \
+			echo "replacing existing git hook: $$hook"; \
 			rm ".git/hooks/$$hook"; \
 		else \
-			echo "applying hook: $$hook"; \
+			echo "applying git hook: $$hook"; \
 		fi; \
 		\
-		ln -s "../../$(GIT_HOOKS_DIR)/$$hook" ".git/hooks/$$hook"; \
+		ln -s "../../$(GIT_HOOK_DIR)/$$hook" ".git/hooks/$$hook"; \
 	done
 
 git-submodule-update:
