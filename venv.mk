817115f31898fcc83a11ed02a31eec4ead9081e9
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-12 17:56:40 -0800

    add venv-force-install-deps

--- venv.mk
+++ venv.mk
@@ -2,7 +2,7 @@ VENV_DIR = .venv
 VENV_ACTIVATE = activate
 VENV_REQUIREMENTS = requirements.txt
 
-.PHONY: venv-active venv-create venv-clean venv-install-deps venv-setup venv-venv-dir-defined venv-venv-activate-defined venv-venv-requirements-defined venv-python-defined
+.PHONY: venv-active venv-create venv-clean venv-force-install-deps venv-install-deps venv-setup venv-venv-dir-defined venv-venv-activate-defined venv-venv-requirements-defined venv-python-defined
 
 venv-python-defined:
 	@ [ -n '$(PYTHON)' ] || (echo 'PYTHON not specified in makefile'; exit 1)
@@ -27,11 +27,19 @@ venv-clean: venv-venv-dir-defined
 	@ [ -z "$$VIRTUAL_ENV" ] || (echo 'venv is active, run: deactivate'; exit 1)
 	@ rm -rf $(VENV_DIR)
 
+venv-force-install-deps: venv-create venv-venv-requirements-defined
+	@ \
+	. ./$(VENV_DIR)/bin/activate && ( \
+		if [ -f './$(VENV_REQUIREMENTS)' ]; then \
+			python -m pip install -r ./$(VENV_REQUIREMENTS) && md5sum './$(VENV_REQUIREMENTS)' >./$(VENV_DIR)/requirements.md5; \
+		fi \
+	)
+
 venv-install-deps: venv-create venv-venv-requirements-defined
 	@ \
 	. ./$(VENV_DIR)/bin/activate && ( \
 		if [ -f './$(VENV_REQUIREMENTS)' ]; then \
-			[ -f './$(VENV_DIR)/requirements.md5' ] && [ -n $$(md5sum './$(VENV_REQUIREMENTS)' | diff - ./$(VENV_DIR)/requirements.md5) ] || python -m pip install -r ./$(VENV_REQUIREMENTS) && md5sum './$(VENV_REQUIREMENTS)' >./$(VENV_DIR)/requirements.md5; \
+			[ -f './$(VENV_DIR)/requirements.md5' ] && [ -n $$(md5sum './$(VENV_REQUIREMENTS)' | diff - ./$(VENV_DIR)/requirements.md5) ] || make venv-force-install-deps; \
 		fi \
 	)
 
